1b40ed7a9bd21fbcadcb51203bcec34c4019bf95
Kunji Lal Meena <kunjilal976@gmail.com>
2024-03-30 22:46:33 +0530

    create and run the server

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+// mongoose.connect("mongodb+srv://kunjilal976:meena@976@mern-project.urlr4z2.mongodb.net/?retryWrites=true&w=majority&appName=mern-project")
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("'connected to mongoDB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+const app=express();
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
