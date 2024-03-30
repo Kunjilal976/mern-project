d192504e8134d5a5ef97103fede150d6ec6edae1
Kunji Lal Meena <kunjilal976@gmail.com>
2024-03-30 23:47:54 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 // mongoose.connect("mongodb+srv://kunjilal976:meena@976@mern-project.urlr4z2.mongodb.net/?retryWrites=true&w=majority&appName=mern-project")
 mongoose.connect(process.env.MONGO)
@@ -15,4 +16,6 @@ const app=express();
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
-})
+});
+
+app.use('/api/user',userRouter);
