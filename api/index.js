4ebb42dd1fad3341bcf3d50290419cac0282e056
Kunji Lal Meena <kunjilal976@gmail.com>
2024-03-31 01:03:14 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 // mongoose.connect("mongodb+srv://kunjilal976:meena@976@mern-project.urlr4z2.mongodb.net/?retryWrites=true&w=majority&appName=mern-project")
 mongoose.connect(process.env.MONGO)
@@ -13,9 +14,11 @@ mongoose.connect(process.env.MONGO)
 });
 
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter)
