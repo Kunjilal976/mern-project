45c98dd72da1f1200c0d64517183d86600cb8beb
Kunji Lal Meena <kunjilal976@gmail.com>
2024-04-01 15:42:01 +0530

    add redux persist

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,58 +1,79 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
-    const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false); 
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center text-white font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.id]: e.target.value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true); 
-        try {
-            const res = await fetch('/api/auth/signin', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData)
-            });
-            const data = await res.json();
-            if (data.success === false) {
-                setError(data.message);
-            } else {
-                setError(null); 
-                navigate('/');
-            }
-        } catch (error) {
-            setError(error.message); 
-        }
-        setLoading(false); 
-      };
-
-    return (
-        <div className="p-3 w-[800px] h-[500px] mx-auto border my-6 border-blue-900 shadow-lg bg-zinc-800 rounded-lg">
-            <h1 className="text-3xl text-center text-white font-semibold my-7">SignIn</h1>
-
-            <form onSubmit={handleSubmit} className="flex flex-col gap-7">
-                <input type="email" placeholder="Email" className="border p-3 rounded-lg" id="email" onChange={handleChange} />
-                <input type="password" placeholder="Password" className="border p-3 rounded-lg" id="password" onChange={handleChange} />
-                <button disabled={loading} className={`bg-blue-500 p-3 rounded-lg uppercase hover:opacity-95 ${loading ? 'cursor-not-allowed' : ''}`}>
-                    {loading ? 'Loading...' : 'Sign In'}
-                </button>
-            </form>
-
-            {error && <p className="text-red-500">{error}</p>} {/* Display error message */}
-            
-            <div className="flex gap-2 mt-5 text-white">
-                <p>Dont have an account?</p>
-                <Link to="/sign-up" className="text-blue-700">Sign Up</Link>
-            </div>
-        </div>
-    );
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p className='text-white'>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
+  );
 }
