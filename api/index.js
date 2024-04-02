2043802255ca2eb7658e90f5bd94e6aa4f0b9a44
Kunji Lal Meena <kunjilal976@gmail.com>
2024-04-02 16:38:15 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 // mongoose.connect("mongodb+srv://kunjilal976:meena@976@mern-project.urlr4z2.mongodb.net/?retryWrites=true&w=majority&appName=mern-project")
 mongoose.connect(process.env.MONGO)
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO)
 
 const app=express();
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
