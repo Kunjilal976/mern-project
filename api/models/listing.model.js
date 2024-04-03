9255c7076d71960d9e5fb53bf61ab63780d4dda5
Kunji Lal Meena <kunjilal976@gmail.com>
2024-04-03 18:48:57 +0530

    complete create listing page functionality

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -1,61 +1,69 @@
 import mongoose from 'mongoose';
 
 const listingSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        parking: {
+            type: Boolean,
+            required: true,
+        },
+        furnished: {
+            type: Boolean,
+            required: true,
+        },
+        wifi: {
+            type: Boolean,
+            required: true,
+        },
+        cctv: {
+            type: Boolean,
+            required: true,
+        },
+        geyser: {
+            type: Boolean,
+            required: true,
+        },
+        privateBathroom: {
+            type: Boolean,
+            required: true,
+        },
+        bedrooms: {
+            type: Number,
+            required: true,
+        },
+        regularPrice: {
+            type: Number,
+            required: true,
+        },
+        discountPrice: {
+            type: Number,
+            required: true,
+        },
+        imageUrls: {
+            type: Array,
+            required: true,
+        },
+        userRef: {
+            type: String,
+            required: true,
+        },
     },
-    description: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    regularPrice: {
-      type: Number,
-      required: true,
-    },
-    discountPrice: {
-      type: Number,
-      required: true,
-    },
-    bathrooms: {
-      type: Number,
-      required: true,
-    },
-    bedrooms: {
-      type: Number,
-      required: true,
-    },
-    furnished: {
-      type: Boolean,
-      required: true,
-    },
-    parking: {
-      type: Boolean,
-      required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    offer: {
-      type: Boolean,
-      required: true,
-    },
-    imageUrls: {
-      type: Array,
-      required: true,
-    },
-    userRef: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true } // save the time of creation and update
 );
 
 const Listing = mongoose.model('Listing', listingSchema);
